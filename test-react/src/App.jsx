fde2222ee18005dd88b755913743cf5b54f95113
bornohin2019 <bornohin2019@gmail.com>
2025-01-14 13:27:57 +0600

    react connect

---
+++ test-react/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Navber from './Navber'
+import Navber2 from './Navber2'
+
+function App() {
+
+  return (
+    <>
+    <Navber></Navber>
+
+      <div>
+        <h1>Hey hena</h1>
+        </div>
+        <Navber2></Navber2>
+    </>
+  )
+}
+
+export default App
